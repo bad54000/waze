0cb72571530688a0da51ff47c1c866d712e0a3c2
Dany Badoinot <dany@conselio.com>
2016-09-11 15:56:12 +0200

    ! index

--- www/index.php
+++ www/index.php
@@ -1,10 +1,16 @@
 <?php
 
 if (!isset($_GET['track'])) {
-	include 'live.html';
+	if (file_exists('live.html')) {
+		include 'live.html';
+	} else {
+		echo "No live template";
+	}
 } else {
 	$track = $_GET['track'];
 	if (file_exists($track.'.txt')) {
 		echo file_get_contents($track.'.txt');
+	} else {
+		echo "No track found";
 	}
 }
